8d9fd335343ef411012bc9539802c8af256fdadd
Branko Kotur <brankokotur@Brankos-Mac-Pro.local>
2014-07-01 19:32:34 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Branko Kotur on 7/1/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
