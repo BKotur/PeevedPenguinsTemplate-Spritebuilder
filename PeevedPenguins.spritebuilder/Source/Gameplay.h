c8fb4619036b2d7c6a033f7c870ae597f896ed6b
Branko Kotur <brankokotur@Brankos-Mac-Pro.local>
2014-07-02 20:16:16 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,15 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Branko Kotur on 7/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+
+
+@end
