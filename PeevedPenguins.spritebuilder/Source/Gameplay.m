42b8d6621c40e822d645c40382cf33711e00e170
Branko Kotur <brankokotur@Brankos-Mac-Pro.local>
2014-07-05 14:22:09 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -113,7 +113,7 @@
 }
 
 - (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
-    
+    CCLOG(@"Something collided with a seal!");
 }
 
 -(void)launchPenguin {
